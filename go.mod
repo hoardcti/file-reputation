0c9725097dc38b48c63ef30ac5d40408164ec0cb
Reece Harris <me+git@reeceharris.net>
2026-09-18 00:12:09 +0100

    empty

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/hoardcti/file-reputation
-
-go 1.26.0
-
-require github.com/joho/godotenv v1.5.1
--- internal/devenv/devenv_prod.go
+++
@@ -1,6 +0,0 @@
-//go:build !dev
-
-package devenv
-
-// Load is a no-op in production builds, as environment variables are expected to be set externally.
-func Load(string) error { return nil }
